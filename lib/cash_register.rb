697c2909eb194a0b149825dde1a498c158fb33c8
edmundo papi <edmundopierre@yahoo.com>
2020-06-28 10:22:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+require "pry"
+def CashRegister
 
+attr_accessor :total, :items, :discount , :last_transaction
+# binding.pry
+
+  def initialize( discount = 0)
+        @total = 0
+        @discount = discount
+        @items =[]
+
+
+  end
+  def add_item(prices,title,quantity)
+    @total = prices *quantity
+  end
+  #def total
+    #  @total = 100
+      #return @total
+  #end
+end
