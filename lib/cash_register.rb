11e194c015cce2d43d858f6cd8a6aabd8e857740
edmundo papi <edmundopierre@yahoo.com>
2020-06-28 11:16:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,25 +1,5 @@
-require "pry"
-def CashRegister
-
-attr_accessor :total, :items, :discount , :last_transaction
-# binding.pry
-
-  def initialize( discount = 0)
-        @total = 0
-        @discount = discount
-        @items =[]
+class CashRegister
 
 
-  end
-  def add_item(prices,title,quantity)
-    self.total = @total+(quantity * prices)
-    quantity.times do
-      @items << title
-    end
 
-  end
-  #def total
-    #  @total = 100
-      #return @total
-  #end
 end
